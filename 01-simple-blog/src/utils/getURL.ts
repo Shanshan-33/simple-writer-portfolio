464742a507e18c931f9f18e18c3c761b4a6e7392
何姗姗 <heshanshan@faw.com.cn>
2025-06-19 11:09:27 +0800

    Added GitHub Actions workflow

--- 01-simple-blog/src/utils/getURL.ts
+++ 01-simple-blog/src/utils/getURL.ts
@@ -1,12 +1,13 @@
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
-export function getURL(data: any): string{
-    if(!data) return 'url';
+export function getURL(data: any): string {
+  if (!data) return "url";
 
-    if(data?.metadata || data?.html){
-        const $ = cheerio.load(data?.html);
-        const url = data?.metadata.url || $('meta[property="og:url"]').attr('content');
-        return url;
-    }
-    return 'url';
+  if (data?.metadata || data?.html) {
+    const $ = cheerio.load(data?.html);
+    const url =
+      data?.metadata.url || $('meta[property="og:url"]').attr("content");
+    return url;
+  }
+  return "url";
 }
